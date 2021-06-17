69b9b3c3ca52e47d5c0a710a712c89b2fdaf5bd4
yuchengH1988 <yucheng.h.1988@gmail.com>
2021-06-17 14:42:48 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
 const express = require('express')
-const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
-const methodOverride = require('method-override')
 const bcrypt = require('bcryptjs')
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+const mongoose = require('mongoose')
+require('./config/mongoose')
 
 const app = express()
-const PORT = 3000
+const PORT = process.env.PORT
 
-app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
-app.set('view engine', 'hbs')
 app.use(bodyParser.urlencoded({ extended: true }))
-app.use(methodOverride('_method'))
+
 
 app.get('/', (req, res) => {
   res.send('hello world')
---
+++ config/mongoose.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose')
+
+const MONGODB_URI = process.env.MONGODB_URI
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
+
+// 取得資料庫連線狀態
+const db = mongoose.connection
+
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
+
+module.exports = db
